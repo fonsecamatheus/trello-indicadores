caa8cec1abd2020e63c15e58a1c3587bac7af17e
fonseca-matheus <matheusrdf1997@hotmail.com>
2024-06-20 08:46:54 -0400

    atualizando data_extract

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ from src.data_extract import DataExtract
 from src.http_requester import CardsApi, ListsApi, MembersApi
 
 if __name__ == "__main__":
+    cards_api = CardsApi()
+    response_cards_api = cards_api.http_response()
+    data_cards = DataExtract(response_cards_api)
+    cards_list = data_cards.data_cards_extract()
+
+    lists_api = ListsApi()
+    response_lists_api = lists_api.http_response()
+    data_list = DataExtract(response_lists_api)
+    lists_list =  data_list.data_lists_extract()
+
     members_api = MembersApi()
-    response = members_api.http_response()
-    data_members = DataExtract(response)
-    members_lists = data_members.data_members_extract()
-    print(members_lists)
+    response_members_api = members_api.http_response()
+    data_members = DataExtract(response_members_api)
+    members_list = data_members.data_members_extract()
+
--- src/data_extract.py
+++ src/data_extract.py
@@ -5,16 +5,16 @@ from typing import Any, Dict, List
 class DataExtract:
     def __init__(self, response: List[Dict[str, Any]]) -> None:
         self.response = response
-        self.board_lists = []
+        self.cards_lists = []
         self.lists_lists = []
         self.members_lists = []
 
-    def data_board_extract(self) -> List[Dict[str, Any]]:
+    def data_cards_extract(self) -> List[Dict[str, Any]]:
         for i in self.response:
             if 'checkItems' in i['badges'] and 'checkItemsChecked' in i['badges']:
                 missing_checks = i['badges']['checkItems'] - i['badges']['checkItemsChecked']
                 
-                data_board_dict = {
+                data_cards_dict = {
                     'data_inicio': i.get('start'),
                     'data_termino': i.get('due'),
                     'id_checklist': i.get('idChecklists'),
@@ -25,8 +25,8 @@ class DataExtract:
                     'checks_faltantes': missing_checks
                 }
 
-                self.board_lists.append(data_board_dict)
-        return self.board_lists
+                self.cards_lists.append(data_cards_dict)
+        return self.cards_lists
     
     def data_lists_extract(self) -> List[Dict[str, str]]:
         for i in self.response:  
@@ -40,7 +40,6 @@ class DataExtract:
     
     def data_members_extract(self) -> List[Dict[str, str]]:
         for i in self.response:
-            print(type(i['id']))
             data_members_dict = {
                 i.get('id') : i.get('fullName')
             }
